5c25e5fff425b142089ed475eb24214e15e33662
Eugene <eugenefung79@gmail.com>
2023-04-06 21:39:41 -0400

    fix ace logic bug

--- js/app.js
+++ js/app.js
@@ -101,11 +101,10 @@ function initRound() {
 function init() {
   ace = false
   deck =[
-    "dA",
-    "hA",'h09',
-    "cA","cQ",'c09',
-    "sA","sQ", 's09'
-  ]
+    "dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02",
+    "hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02",
+    "cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02",
+    "sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
   cash = 100
   bet = 0
   blackjack = false
@@ -260,25 +259,21 @@ function drawCard() {
         }
       } 
     }
-  playerSum += cardPullValue
+    playerSum += cardPullValue
   const isAce = playerHand.some(c =>{
     return c.toString()[1]==='A'
   })
     if (playerSum < 12 && isAce === true) {
       playerSum += 10
     } 
-      if (playerSum > 21 && ace === false && isAce === true) {
-      playerSum -= 10
+    if (playerSum > 21 && ace === false && isAce === true) {
       ace = true
+      playerSum -= 10
     }
-    if (playerSum === 21) {
-        blackjack = true
-    } 
+    
     playerSumEl.textContent = 'Player: ' + playerSum
     renderPlayer(cardPicked)
     checkLose()
-    checkTie()
-    checkBlackjack()
   }
 }
 
@@ -308,12 +303,14 @@ function dealerDrawCard() {
     })
     if (dealerSum < 12 && isAce === true) {
       dealerSum += 10
-    } else if (dealerSum > 21 && ace === false && isAce === true) {
+    } 
+    if (dealerSum > 21 && ace === false && isAce === true) {
       dealerSum -= 10 
       ace = true
+    } else {
+      cardPullValue = cardPullValue
     }
     checkTie()
-    roundEnd()
     renderBet()
       
     dealerSumEl.textContent = 'Dealer: ' + dealerSum
@@ -325,6 +322,9 @@ function stand() {
   renderDealerRemoveFacedown()
   while (playerSum >= dealerSum && dealerSum != 21) {
     dealerDrawCard()
+  }
+  if (playerSum === 21) {
+    blackjack = true
   }
   checkTie()
   checkWin()
